e25f8acb421ab5fbdffa9deb45f02be648c313cd
ExchangeLarry <43578377+ExchangeLarry@users.noreply.github.com>
2018-11-03 03:38:13 +0100

    Update constants.js

--- server/constants.js
+++ server/constants.js
@@ -26,7 +26,7 @@ exports.share = {
    
    TRADE_MAIN_COIN: "Bitcoin",
    TRADE_MAIN_COIN_TICKER: "BTC",
-   TRADE_DEFAULT_PAIR: "Aegeus"
+   TRADE_DEFAULT_PAIR: "MasterCoin"
 };
 
 exports.my_port = process.env.PORT || 40080;
