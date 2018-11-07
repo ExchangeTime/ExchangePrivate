6b9adc9c4c1bf1ecea955734229b4f9f2c33cd83
ExchangeLarry <43578377+ExchangeLarry@users.noreply.github.com>
2018-11-07 13:06:39 +0100

    Update private_constants.js

--- server/modules/private_constants.js
+++ server/modules/private_constants.js
@@ -1,12 +1,12 @@
 'use strict';
 
 exports.recaptcha_priv_key = 'YOUR_GOOGLE_RECAPTCHA_PRIVATE_KEY';
-exports.password_private_suffix = 'LONG_RANDOM_STRING1';
+exports.password_private_suffix = 'LongTimeSecretCode99522';
 exports.SSL_KEY = '../ssl_certificates/privkey.pem'; //change to your ssl certificates private key
 exports.SSL_CERT = '../ssl_certificates/fullchain.pem'; //change to your ssl certificates fullchain
 
 exports.walletspassphrase = {
-    'AEG' : 'randomlongstringSAMEASINFILEprivateconstants',
-    'BTC' : 'randomlongstringSAMEASINFILEprivateconstants',
-    'DOGE' : 'LONG_RANDOM_STRING4'
+    'AEG' : 'VerySecretCodeTestRun524',
+    'BTC' : 'VerySecretCodeTestRun525',
+    'DOGE' : 'VerySecretCodeTestRun526'
 };
