e63589100bb11ff4466245d78d50b50df1d989e2
ExchangeLarry <43578377+ExchangeLarry@users.noreply.github.com>
2018-11-08 11:56:04 +0100

    Update private_constants.js

--- server/modules/private_constants.js
+++ server/modules/private_constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.recaptcha_priv_key = 'YOUR_GOOGLE_RECAPTCHA_PRIVATE_KEY';
+exports.recaptcha_priv_key = '6Lcmc3gUAAAAABLFZyScT5A3qpza7a3_WUB6GwJS';
 exports.password_private_suffix = 'LongTimeSecretCode99522';
 exports.SSL_KEY = '../ssl_certificates/privkey.pem'; //change to your ssl certificates private key
 exports.SSL_CERT = '../ssl_certificates/fullchain.pem'; //change to your ssl certificates fullchain
