1d74b56c0e899a80cbd4fd20189b89aaa1ffaea0
ExchangeLarry <43578377+ExchangeLarry@users.noreply.github.com>
2018-11-08 20:20:14 +0100

    Update private_constants.js

--- server/modules/private_constants.js
+++ server/modules/private_constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.recaptcha_priv_key = '6Lcmc3gUAAAAABLFZyScT5A3qpza7a3_WUB6GwJS';
+exports.recaptcha_priv_key = '6LeGhXkUAAAAADoqi16ykPuHW7u4ctomNbvvbmVr';
 exports.password_private_suffix = 'LongTimeSecretCode99522';
 exports.SSL_KEY = '../ssl_certificates/privkey.pem'; //change to your ssl certificates private key
 exports.SSL_CERT = '../ssl_certificates/fullchain.pem'; //change to your ssl certificates fullchain
